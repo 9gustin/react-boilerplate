a6db5fa78bc0d6f727bbfa40bdb157a5650bccfc
Agustin Vazquez <agustinvazquez453@gmail.com>
2021-06-09 20:08:07 -0300

    make pwa

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 import App from 'pages';
 import 'config/i18n';
 
+import * as sw from './serviceWorkerRegistration';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
+
+sw.register();
